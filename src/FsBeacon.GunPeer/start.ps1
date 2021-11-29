11340d91015d907141404d0cc1693f34384e8635
fc1943s <root@fc1943s.dev>
2021-11-29 15:15:38 -0300

    gun cert

--- src/FsBeacon.GunPeer/start.ps1
+++ src/FsBeacon.GunPeer/start.ps1
@@ -1,8 +1,10 @@
 yarn install
 
-yarn mkcert -install
+# yarn mkcert -install
 
 [System.IO.Directory]::CreateDirectory('ssl')
-yarn mkcert --cert-file=./ssl/$env:FSBEACON_DOMAIN.pem --key-file=./ssl/$env:FSBEACON_DOMAIN-key.pem $env:FSBEACON_DOMAIN
+# yarn mkcert --cert-file=./ssl/$env:FSBEACON_DOMAIN.pem --key-file=./ssl/$env:FSBEACON_DOMAIN-key.pem $env:FSBEACON_DOMAIN
+yarn mkcert create-ca --cert=./ssl/ca.pem --key=./ssl/ca-key.pem
+yarn mkcert create-cert --ca-cert=./ssl/ca.pem --ca-key=./ssl/ca-key.pem --cert=./ssl/$env:FSBEACON_DOMAIN.pem --key=./ssl/$env:FSBEACON_DOMAIN-key.pem --domains $env:FSBEACON_DOMAIN
 
 node server.js
